c19c899cdbc511fa32a870a3190e4bc8db0c5032
Yamin Yamin <naifalhumaidi@proton.me>
2024-08-28 13:07:21 +0300

    add ProductCard component

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+interface IProps {
+  
+}
+ const ProductCard: FC<IProps> = ({}) => {
+  return (
+    <>
+      <h2>ProductCard</h2>
+    </>
+  )
+}
+export default ProductCard;
