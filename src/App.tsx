5294b655fda39fff394374868fdf064151a00ed7
Yamin Yamin <naifalhumaidi@proton.me>
2024-09-02 23:59:11 +0300

    nothing

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ function App() {
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 m-9">
         <ProductCard/>
-        <ProductCard/>
-        <ProductCard/>
       </div>
     </>
   )
