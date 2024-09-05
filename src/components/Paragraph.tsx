222734e07abfed97baa842e4177f2288a76be95b
Yamin Yamin <naifalhumaidi@proton.me>
2024-09-05 23:41:18 +0300

    add paragraph component

---
+++ src/components/Paragraph.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+interface IProps {
+  
+}
+ const Paragraph: FC<IProps> = ({}) => {
+  return (
+    <>
+      Paragraph Component
+    </>
+  )
+}
+export default Paragraph;
