f50da99589eedcc1176fa57121ad573883ba34dc
Yamin Yamin <naifalhumaidi@proton.me>
2024-09-07 23:47:03 +0300

    Add EditDialog Compnent

---
+++ src/components/EditDialog.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+interface IProps {
+  
+}
+ const EditDialog: FC<IProps> = ({}) => {
+  return (
+    <>
+      Edit Dialog Component
+    </>
+  )
+}
+export default EditDialog;
