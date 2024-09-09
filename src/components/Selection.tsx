ab134359bd60ad9a41cf612e7d03acbe9d3f2972
Yamin Yamin <naifalhumaidi@proton.me>
2024-09-09 23:57:09 +0300

    Add Selection Component

---
+++ src/components/Selection.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+interface IProps {
+  
+}
+ const Selection: FC<IProps> = ({}) => {
+  return (
+    <>
+      Selection Component
+    </>
+  )
+}
+export default Selection;
