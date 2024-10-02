636fe8f3318e2ee73fc83d1045bfdd6484636698
Yamin Yamin <naifalhumaidi@proton.me>
2024-10-02 23:52:03 +0300

    Nothing

--- src/components/deleteDialog.tsx
+++ src/components/deleteDialog.tsx
@@ -1,12 +1,14 @@
 import { FC } from "react";
 import Button from "./Button";
+import AddProductModal from "./AddProductModal";
 interface IProps {
   
 }
  const deleteDialog: FC<IProps> = ({}) => {
   return (
     <>
-        
+      {/* Temporary */}
+      <AddProductModal/>
     </>
   )
 }
